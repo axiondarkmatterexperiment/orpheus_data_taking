f677aabad52ad4ddf35036f5f14a2dd7b0240599
jtsinnis <77714881+jtsinnis@users.noreply.github.com>
2025-02-06 10:15:14 -0800

    Create magnet_regulator_logging.py

---
+++ magnet_supply_controller/magnet_regulator_logging.py
@@ -0,0 +1,46 @@
+import psycopg2
+
+def create_magnet_regulaor_table():
+    ''' 
+    Set up connection to the psql database
+    '''
+
+    # define the connection to the postgres database
+    # conn = psycopg2.connect(host='192.168.25.2', dbname='postgres', user='admx_master', password='wimpssuck', port=5432)
+    conn = psycopg2.connect(host='192.168.25.2', dbname='orpheus_db', user='postgres', password='axionsrock', port=5432)
+
+    #I think this is just what we use to send commands directly to the postgres command line
+    cur = conn.cursor()
+    
+    cur.execute("""CREATE TABLE IF NOT EXISTS public.magnet_regulator (
+                sensor_name TEXT,
+                "timestamp" TIMESTAMPTZ,
+                val_raw REAL,
+                val_cal REAL
+                );
+                """)
+    
+    conn.commit()
+    
+    cur.close()
+    conn.close()
+
+def log_magnet_regulator_value(sensor_name, timestamp, val_raw, val_cal ):
+    ''' 
+    Set up connection to the psql database
+    '''
+
+    # define the connection to the postgres database
+    # conn = psycopg2.connect(host='192.168.25.2', dbname='postgres', user='admx_master', password='wimpssuck', port=5432)
+    conn = psycopg2.connect(host='192.168.25.2', dbname='orpheus_db', user='postgres', password='axionsrock', port=5432)
+
+    #I think this is just what we use to send commands directly to the postgres command line
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO magnet_regulator (sensor_name, timestamp, val_raw, val_cal) VALUES (%s, %s, %s, %s)",
+                (sensor_name, timestamp, val_raw, val_cal))
+    
+    conn.commit()
+    
+    cur.close()
+    conn.close()
