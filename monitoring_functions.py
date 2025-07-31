2482c2e9227969f220521cdb763cad2652ffdaf3
jtsinnis <77714881+jtsinnis@users.noreply.github.com>
2025-07-31 12:17:32 -0700

    Update monitoring_functions.py

--- monitoring_functions.py
+++ monitoring_functions.py
@@ -329,8 +329,9 @@ def transmission_scan(f_center, f_span, na_power=-10, n_avgs=10, if_bw = 1e4):
     PORT=5025
     TIMEOUT=10 #This might need to be changed dependent on the averaging time
 
-    SCPI_string = "*IDN?\n"
-    
+    SCPI_string = "INIT1:CONT?\n"
+    print(query_SCPI(IP_ADDRESS, PORT, TIMEOUT, SCPI_string)[1])
+    SCPI_string = "TRIG:SOUR BUS?\n"
     print(query_SCPI(IP_ADDRESS, PORT, TIMEOUT, SCPI_string)[1])
 
 def query_SCPI(IP_ADDRESS, PORT, TIMEOUT, SCPI_string):
