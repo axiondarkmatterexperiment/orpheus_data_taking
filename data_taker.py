71ae7c1ece2e342af6c60bf0952a7599ddb28d86
jtsinnis <77714881+jtsinnis@users.noreply.github.com>
2025-09-12 12:14:48 -0700

    Delete data_taker.py

--- data_taker.py
+++
@@ -1,5 +0,0 @@
-#This script will run and monitor the experiment. It is the script that will take in DAQ variables such as the
-#na power, digitization duration, etc, (the sorts of things that are set on the Data Taking page for ADMX).
-
-from monitoring_functions import *
-
