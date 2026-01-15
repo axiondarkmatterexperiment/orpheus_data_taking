085e2413e75976738e93cdb7900d930d4b926613
jtsinnis <jt.sinnis@gmail.com>
2026-01-15 13:07:45 -0800

    many changes (in progress)

--- data_taking_functions.py
+++ data_taking_functions.py
@@ -2,8 +2,10 @@
 #assumes table is already made
 import sys
 import os
+import time
 
 import numpy as np
+import io
 
 sys.path.insert(0, 'magnet_supply_controller')
 import psycopg2
@@ -53,6 +55,25 @@ def log_na_scan(scan_type, timestamp, freqs, iq_data):
     cur.close()
     conn.close()
 
+def log_digitization(timestamp, freqs, pows):
+    ''' 
+    Set up connection to the psql database
+    '''
+
+    # define the connection to the postgres database
+    conn = psycopg2.connect(host='192.168.25.2', dbname='orpheus_db', user='postgres', password='axionsrock', port=5432)
+
+    #I think this is just what we use to send commands directly to the postgres command line
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO digitizations (timestamp, freqs, pows) VALUES (%s, %s, %s)",
+                (timestamp, freqs, pows))
+    
+    conn.commit()
+    
+    cur.close()
+    conn.close()
+    
 
 #logs either reflection or transmission scans in a separate table for displaying on grafana.
 #Will log the fit as well. Assumes no fit data but if fit is provided it will log it.
@@ -135,7 +156,7 @@ def switch_rf(setting): #setting values: "transmission", "reflection", "digitize
     elif setting == "reflection":
         ch1="0"
         ch2="1"
-    elif setting == "digitization":
+    elif setting == "digitizer":
         ch1="1"
         ch2="0"
     else:
@@ -157,7 +178,7 @@ def scan_na(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_bw_Hz = 1e4):
     #send the query to the VNA:
     IP_ADDRESS="192.168.25.7"
     PORT=5025
-    TIMEOUT=10 #This might need to be changed dependent on the averaging time
+    TIMEOUT=5 #This might need to be changed dependent on the averaging time
 
     #Sweep setup
     SCPI_string = "SENS1:FREQ:CENT " + str(f_center_GHz*1e9) + "\n"
@@ -190,16 +211,21 @@ def scan_na(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_bw_Hz = 1e4):
     #Take scan data
     SCPI_string = "CALC1:DATA:SDAT?\n" #ask for the IQ data. Format: n*2-1 is real, n*2 is imaginary
     timestamp, iq_raw = query_SCPI(IP_ADDRESS, PORT, TIMEOUT, SCPI_string)
+
     SCPI_string = "SENS1:FREQ:DATA?\n" #ask for the frequency vector
     timestamp, f_raw = query_SCPI(IP_ADDRESS, PORT, TIMEOUT, SCPI_string)
 
-    f_raw = np.fromstring(f_raw, dtype=np.float64, sep=',').tolist()
-    iq_raw = np.fromstring(iq_raw, dtype=np.float64, sep=',').tolist()
-
+    if f_raw and iq_raw:
+        f_raw = np.fromstring(f_raw, dtype=np.float64, sep=',').tolist()
+        iq_raw = np.fromstring(iq_raw, dtype=np.float64, sep=',').tolist()
+    #If the query_SCPI times out then it returns False for the val_raw, in which case I want an empty list.
+    else:
+        iq_raw = []
+        f_raw = []
 
     return f_raw, iq_raw 
 
-def log_transmission_scan(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_bw_Hz = 1e4, fitting=True):
+def log_transmission_scan(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_bw_Hz = 1e4, fitting=True, param_logging=True):
     switch_rf("transmission")
     timestamp = datetime.datetime.now(pytz.timezone('US/Pacific'))
     f, iq = scan_na(f_center_GHz, f_span_GHz, na_power, n_avgs, if_bw_Hz) 
@@ -211,30 +237,35 @@ def log_transmission_scan(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_
     if fitting:
         re, im = iq[::2], iq[1::2]    
         p = np.square(re) + np.square(im)
-        #try:
-        popt, pcov = data_lorentzian_fit(p,f,'transmission')
-        perr = np.sqrt(np.diag(pcov))
-        log_cavity_params("f0_trans",timestamp, float(popt[0]))
-        log_cavity_params("Q_trans",timestamp, float(popt[1]))
-        log_cavity_params("dy_trans",timestamp, float(popt[2]))
-        log_cavity_params("C_trans",timestamp, float(popt[3]))
-         
-        log_cavity_params("f0_err_trans",timestamp, float(perr[0]))
-        log_cavity_params("Q_err_trans",timestamp, float(perr[1]))
-        log_cavity_params("dy_err_trans",timestamp, float(perr[2]))
-        log_cavity_params("C_err_trans",timestamp, float(perr[3]))
-       
-        p_fit = func_pow_transmitted(f, popt[0], popt[1], popt[2], popt[3])
+        try:
+            popt, pcov = data_lorentzian_fit(p,f,'transmission')
+            perr = np.sqrt(np.diag(pcov))
+            p_fit = func_pow_transmitted(f, popt[0], popt[1], popt[2], popt[3])
+            p_fit = p_fit.tolist()
+        except:
+            popt = [np.nan, np.nan, np.nan, np.nan]
+            perr = [np.nan, np.nan, np.nan, np.nan]
+            p_fit = []
+            print("fit failed")
+        if param_logging==True:
+            log_cavity_params("f0_trans",timestamp, float(popt[0]))
+            log_cavity_params("Q_trans",timestamp, float(popt[1]))
+            log_cavity_params("dy_trans",timestamp, float(popt[2]))
+            log_cavity_params("C_trans",timestamp, float(popt[3]))
+             
+            log_cavity_params("f0_err_trans",timestamp, float(perr[0]))
+            log_cavity_params("Q_err_trans",timestamp, float(perr[1]))
+            log_cavity_params("dy_err_trans",timestamp, float(perr[2]))
+            log_cavity_params("C_err_trans",timestamp, float(perr[3]))
 
         p = p.tolist()
-        p_fit = p_fit.tolist()
         log_na_scan_for_display("transmission", timestamp, f, p, p_fit)
         return popt[0], popt[1]
     else:
         log_na_scan_for_display(fitting, "transmission", timestamp, f, p)
         return
 
-def log_reflection_scan(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_bw_Hz = 1e4, fitting=True):
+def log_reflection_scan(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_bw_Hz = 1e4, fitting=True, param_logging=True):
     switch_rf("reflection")
     timestamp = datetime.datetime.now(pytz.timezone('US/Pacific'))
     f, iq = scan_na(f_center_GHz, f_span_GHz, na_power, n_avgs, if_bw_Hz) 
@@ -246,18 +277,26 @@ def log_reflection_scan(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_bw
     if fitting:
         re, im = iq[::2], iq[1::2]
         p = np.square(re) + np.square(im)
-        #try:
-        popt, pcov = data_lorentzian_fit(p,f,'reflection')
-        perr = np.sqrt(np.diag(pcov))
-        log_cavity_params("f0_refl",timestamp, float(popt[0]))
-        log_cavity_params("Q_refl",timestamp, float(popt[1]))
-        log_cavity_params("dy_refl",timestamp, float(popt[2]))
-        log_cavity_params("C_refl",timestamp, float(popt[3]))
-         
-        log_cavity_params("f0_err_refl",timestamp, float(perr[0]))
-        log_cavity_params("Q_err_refl",timestamp, float(perr[1]))
-        log_cavity_params("dy_err_refl",timestamp, float(perr[2]))
-        log_cavity_params("C_err_refl",timestamp, float(perr[3]))
+        try:
+            popt, pcov = data_lorentzian_fit(p,f,'transmission')
+            perr = np.sqrt(np.diag(pcov))
+            p_fit = func_pow_reflected(f, popt[0], popt[1], popt[2], popt[3])
+            p_fit = p_fit.tolist()
+        except:
+            popt = [np.nan, np.nan, np.nan, np.nan]
+            perr = [np.nan, np.nan, np.nan, np.nan]
+            p_fit = []
+            print("fit failed")
+        if param_logging==True:
+            log_cavity_params("f0_refl",timestamp, float(popt[0]))
+            log_cavity_params("Q_refl",timestamp, float(popt[1]))
+            log_cavity_params("dy_refl",timestamp, float(popt[2]))
+            log_cavity_params("C_refl",timestamp, float(popt[3]))
+             
+            log_cavity_params("f0_err_refl",timestamp, float(perr[0]))
+            log_cavity_params("Q_err_refl",timestamp, float(perr[1]))
+            log_cavity_params("dy_err_refl",timestamp, float(perr[2]))
+            log_cavity_params("C_err_refl",timestamp, float(perr[3]))
        
         phase = np.unwrap(np.angle(np.asarray(re)+1j*np.asarray(im)))
         cavity_phase = deconvolve_phase(f, phase)
@@ -265,13 +304,98 @@ def log_reflection_scan(f_center_GHz, f_span_GHz, na_power=-10, n_avgs=16, if_bw
         
         log_cavity_params("beta",timestamp,float(beta))
 
-        p_fit = func_pow_reflected(f, popt[0], popt[1], popt[2], popt[3]) 
-        
         p = p.tolist()
-        p_fit = p_fit.tolist()
         log_na_scan_for_display("reflection", timestamp, f, p, p_fit)
 
         return popt[0], popt[1], beta
     else:
         log_na_scan_for_display(fitting, "reflection", timestamp, f, p)
         return
+    
+def log_transmission_widescan(f_center_GHz, f_span_GHz, na_power=-5, n_avgs=30, if_bw_Hz = 1e4):
+    switch_rf("transmission")
+    timestamp = datetime.datetime.now(pytz.timezone('US/Pacific'))
+    f, iq = scan_na(f_center_GHz, f_span_GHz, na_power, n_avgs, if_bw_Hz)
+    re = iq[0::2]
+    im = iq[1::2]
+    p = np.add(np.square(re),np.square(im)).astype(np.float64)
+    p = p.tolist()
+    log_na_scan("transmission_widescan", timestamp, f, p)
+    return f, p
+
+def set_lo_center_freq(center_freq):
+    IP_ADDRESS = "192.168.25.10"
+    PORT = 5025
+    TIMEOUT = 5
+    write_SCPI(IP_ADDRESS, PORT, TIMEOUT, "FREQ:CW " + str(center_freq) + "\n")
+    #Check that it has been set properly, return false / zero if not:
+    timestamp, f_set = query_SCPI(IP_ADDRESS, PORT, TIMEOUT, "FREQ:CW?\n")
+    if float(f_set) == center_freq:
+        return f_set
+    else:
+        return 0
+
+def digitize(acquisition_time):
+    switch_rf("digitizer")
+    IP_ADDRESS = "192.168.25.8"
+    PORT = 50000
+    TIMEOUT=5
+    set_acq_time_string = "SET:ACQUISITION_TIME " + str(acquisition_time) + "\n"
+    write_SCPI(IP_ADDRESS, PORT, TIMEOUT, set_acq_time_string)
+    time.sleep(0.01)
+    write_SCPI(IP_ADDRESS, PORT, TIMEOUT, "START\n")
+    wait_for_digitization()
+    timestamp, spectrum = query_SCPI(IP_ADDRESS, PORT, TIMEOUT, "GET:LAST_SPECTRUM?\n")
+    spectrum = io.StringIO(spectrum)
+    spectrum = np.genfromtxt(spectrum,dtype=float,delimiter=None)
+    #spectrum = spectrum.T
+    pows = spectrum.tolist()
+    freqs = np.arange(np.size(pows)).tolist()
+    log_digitization(timestamp, freqs, pows)
+    return timestamp, spectrum
+    
+def wait_for_digitization():
+    IP_ADDRESS = "192.168.25.8"
+    PORT = 50000
+    TIMEOUT=5
+
+    digitizing = True
+    while digitizing:
+        timestamp, dig_status = query_SCPI(IP_ADDRESS, PORT, TIMEOUT, "GET:STATUS?\n")
+        dig_status = dig_status[0:-1] #Remove the \n on the end of the returned string
+        print(dig_status)
+        if dig_status == "RUNNING":
+            digitizing = True
+        elif dig_status == "IDLE":
+            digitizing = False
+        time.sleep(1)
+    return
+
+def tune_while_tracking_mode(initial_f0_GHz, initial_span_GHz, tune_distance_cm, tune_increment_cm, measure_coupling=True):
+    from motor_functions import coordinated_motion
+    na_span_GHz = initial_span_GHz
+    na_fc_GHz = initial_f0_GHz
+    num_steps = int(tune_distance_cm/tune_increment_cm)
+    j=0
+    while j<num_steps:
+        #Look at a wider window after tuning to find the f0 and rough estimate of QL. Don't log the measured cavity parameters
+        na_span_GHz=2*na_span_GHz
+        na_fc, current_QL = log_transmission_scan(na_fc_GHz, na_span_GHz, param_logging=False)
+        time.sleep(0.1)
+        na_fc_GHz = na_fc/1e9
+        #Make the window smaller around the approximate peak and retake the transmission scan. Log the measured cavity parameters
+        na_span_GHz = na_span_GHz/2
+        na_fc, current_QL = log_transmission_scan(na_fc_GHz, na_span_GHz, param_logging=False)
+        time.sleep(0.1)
+        na_fc_GHz = na_fc/1e9
+        na_span_GHz = 5*na_fc_GHz/current_QL
+        na_fc, current_QL = log_transmission_scan(na_fc_GHz, na_span_GHz, param_logging=True)
+        time.sleep(0.1)
+        if measure_coupling:
+            log_reflection_scan(na_fc_GHz, na_span_GHz/1.7, param_logging=True)
+        coordinated_motion(tune_increment_cm)
+        time.sleep(0.1)
+        j=j+1
+    return j*tune_increment_cm
+
+
