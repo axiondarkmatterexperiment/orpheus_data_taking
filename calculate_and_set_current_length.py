33398b860fa03ea8b7fd232dc8404b9e6ca8b7e0
jtsinnis <jt.sinnis@gmail.com>
2026-02-06 16:56:51 -0800

    function to set the current length of the cavity based off of f0 measurement and simulations

--- calculate_and_set_current_length.py
+++ calculate_and_set_current_length.py
@@ -1,5 +1,6 @@
 from data_taking_functions import log_transmission_scan
 from fitting_functions import cavity_length_from_f0
+import numpy as np
 f_center_GHz = input("input the current f0 in GHz: ")
 f_center_GHz = float(f_center_GHz)
 f_span_GHz = input("input the na f_span_GHz you want in GHz: ")
@@ -13,4 +14,12 @@ if ok=="":
     f0,Q = log_transmission_scan(f_center_GHz,f_span_GHz)
     f0,Q = log_transmission_scan(f0/1e9,f_span_GHz)
 
-
+    f0_GHz = f0/1e9
+    length_cm = cavity_length_from_f0(f0_GHz)
+    print("the measured f0 is " + str(f0_GHz) + "GHz")
+    print("the calculated cavity length is " + str(length_cm) + "cm")
+    ok2 = input("are these values ok? Hit enter if so, otherwise, type anything then hit enter. If OK, the value will be saved to cavity_current_length.txt")
+    if ok2=="":
+        np.savetxt("cavity_current_length.txt",np.asarray([length_cm]))
+    else:
+        print("Doing nothing. The cavity_current_length.txt file remains unchanged.")
