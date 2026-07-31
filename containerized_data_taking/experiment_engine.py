f2bc29a89e60b0e8244c39b82f53092cdd1740e6
jtsinnis <jt.sinnis@gmail.com>
2026-07-31 10:13:49 -0700

    fixing bugsY

--- containerized_data_taking/experiment_engine.py
+++ containerized_data_taking/experiment_engine.py
@@ -67,15 +67,15 @@ def take_data():
                 na_fc_temp, transmission_Q_temp = log_transmission_scan(na_fc, na_span, data_id=data_taking_id)
                 #Only update the values if they are not NaNs
                 if np.isfinite(na_fc_temp):
-                    na_fc = na_fc_temp
+                    na_fc = na_fc_temp/1e9
                 if np.isfinite(transmission_Q_temp):
                     transmission_Q = transmission_Q_temp
                 timestamp = dt.now(pytz.timezone('US/Pacific'))
                 last_task = "Transmission:"+str(timestamp)
                 with state.lock:
-                    state.na_fc = float(na_fc/1e9)
+                    state.na_fc = float(na_fc)
                     #na_fc is changed by the data taker sometimes in order to guide the NA. This value is only ever set by the transmission scan, and is used for setting the fc of the digitization
-                    state.transmission_f0 = float(na_fc/1e9) 
+                    state.transmission_f0 = float(na_fc) 
                     state.transmission_Q = float(transmission_Q)
                     state.na_span = float(na_span)
                     state.last_task = last_task
